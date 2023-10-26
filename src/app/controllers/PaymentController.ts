1e30bb542fba293a83ae3f54903f2287d9e3615f
Joas-Assuncao <joassalvior@gmail.com>
2023-10-26 19:06:46 -0300

    implements payment controller

--- src/app/controllers/PaymentController.ts
+++ src/app/controllers/PaymentController.ts
@@ -1,19 +1,39 @@
+import { AxiosResponse } from "axios";
+import { Request, Response } from "express";
+
+import { IPaymentData } from "../models/paymentData";
+import { apiService } from "../services/axios";
+
 class PaymentController {
-  async index() {
-    return ["payments"];
+  async findAll(req: Request, res: Response) {
+    try {
+      const response: AxiosResponse<IPaymentData> = await apiService({
+        method: "GET",
+        url: "/payments",
+      });
+
+      res.status(200).json(response.data);
+    } catch (err) {
+      // 500 pois é um erro em outra api(servidor)
+      res.status(500).json(err);
+    }
   }
 
-  async store() {
-    return ["payments"];
-  }
+  async create(req: Request, res: Response) {
+    try {
+      const response: AxiosResponse<IPaymentData> = await apiService({
+        method: "POST",
+        url: "/payments",
+        data: req.body,
+      });
 
-  async update() {
-    return ["payments"];
-  }
-
-  async delete() {
-    return;
+      res.status(201).json(response.data);
+    } catch (err: any) {
+      console.log(err.response.data);
+      // 500 pois é um erro em outra api(servidor)
+      res.json(err);
+    }
   }
 }
 
-module.exports = new PaymentController();
+export default new PaymentController();
