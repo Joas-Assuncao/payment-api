a3c272312444f1f682a0a0e4883ab2beff4a3868
Joas-Assuncao <joassalvior@gmail.com>
2023-10-26 19:07:32 -0300

    improve server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
 import cors from "cors";
-import express, { Request, Response, Router } from "express";
+import express from "express";
 
 import { errorHandler } from "./app/middlewares/errorHandler";
 
-const routes = require("./routes");
+import routes from "./routes";
 
 const app = express();
 
 app.use(express.json());
-app.use(cors);
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use(routes);
 app.use(errorHandler);
 
